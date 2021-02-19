8b54c969431d795d0351e01cc446099ff1e1520c
bmertali <mertalibarin@gmail.com>
2021-02-19 15:45:15 +0300

    Last commit for todoApp

---
+++ todoApp/src/model.ts
@@ -0,0 +1,26 @@
+export class Model {
+    user;
+    items;
+
+    constructor(){
+        this.user = "Mert",
+        this.items = [
+            new TodoItem("Spor",true),
+            new TodoItem("Kahvaltı",false),
+            new TodoItem("Kitap Okumak",false),
+            new TodoItem("Sinema",false)
+
+        
+          ]
+    }
+}
+
+export class TodoItem {
+    description;
+    action;
+
+    constructor(description,action){
+        this.description = description
+        this.action = action
+    }
+}
