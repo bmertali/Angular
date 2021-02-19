f1f7ba8fde4afbf7e0a7274bae96755945f11edb
bmertali <mertalibarin@gmail.com>
2021-02-19 19:28:01 +0300

    Movie App Ders 330 Sonu

--- MovieApp/src/app/movies/movies.component.ts
+++ MovieApp/src/app/movies/movies.component.ts
@@ -1,10 +1,18 @@
 import { Component } from "@angular/core";
+import { Movies } from '../movie.datasource'
+import { Movie } from '../movie'
 
 @Component({
     selector: 'movies', // <movies></movies>
-    templateUrl: 'movies.component.html'
+    templateUrl: 'movies.component.html',
 })
 
 export class MoviesComponent {
+    title = 'Movie List';
+    movies = Movies
+    selectedMovie: Movie
 
+    onSelect(movie: Movie) : void {
+        this.selectedMovie = movie
+    }
 }
